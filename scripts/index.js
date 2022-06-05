407bd2d4487e9db32dc339177f9127955fe82c0a
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-05 17:37:11 +0300

    feat(5): добавить стили для меню основных разделов

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 const burgerMenuButton = document.querySelector('.header__menu-button');
-const menu = document.querySelector('.header__links');
+const menu = document.querySelector('.header__menu');
 
 
 burgerMenuButton.addEventListener('click', function() {
   burgerMenuButton.classList.toggle('header__menu-button_type_close');
-  menu.classList.toggle('header__links_opened');
+  menu.classList.toggle('header__menu_opened');
 });
